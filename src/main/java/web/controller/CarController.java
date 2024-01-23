f1de77f4e7a36a7d65dce4c234213827bd698b83
Andrey Paskhin <andrey60906@mail.ru>
2024-01-23 23:06:31 +0300

    Выполнено

---
+++ src/main/java/web/controller/CarController.java
@@ -0,0 +1,25 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.service.CarService;
+import web.service.CarServiceImpl;
+
+@Controller
+public class CarController {
+    private final CarService carService;
+
+    @Autowired
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    @GetMapping(value = "/cars")
+    public String index(@RequestParam(value = "count", defaultValue = "5") int number, Model model) {
+        model.addAttribute("cars", carService.getNumberOfCars(number));
+        return "cars";
+    }
+}
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,20 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import web.dao.CarDAO;
+import web.model.Car;
+
+import java.util.List;
+@Service("carServiceImpl")
+public class CarServiceImpl implements CarService{
+    public CarDAO carDao;
+    @Autowired
+    public CarServiceImpl(CarDAO carDao){
+        this.carDao = carDao;
+    }
+    @Override
+    public List<Car> getNumberOfCars(int number) {
+        return carDao.getAllCars().stream().limit(number).toList();
+    }
+}
